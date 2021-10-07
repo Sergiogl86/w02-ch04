30771e844c3d89788e7d2d2c402950c20c6e2493
Sergio Garcia <sergio.gl86@gmail.com>
2021-10-07 22:09:00 +0200

    Se completa la funcion map

--- skylab-array.js
+++ skylab-array.js
@@ -31,25 +31,52 @@ class SkylabArray {
     }
     return undefined;
   }
+
+  filter(value) {
+    const auxArray = new SkylabArray();
+    const filterFunction = value;
+    debugger;
+    for (let i = 0; i < this.length; i++) {
+      if (filterFunction(this[i])) {
+        auxArray.push(this[i]);
+      }
+    }
+    if (auxArray.length === 0) {
+      return undefined;
+    } else {
+      return auxArray;
+    }
+  }
+
+  map(value) {
+    const auxArray = new SkylabArray();
+    const mapFunction = value;
+    for (let i = 0; i < this.length; i++) {
+      auxArray.push(mapFunction(this[i]));
+    }
+    return auxArray;
+  }
 }
 
 const prueba = new SkylabArray();
 
-prueba.push("casa");
+prueba.push("caca");
 
-prueba.push("toro");
+prueba.push(25);
 
-prueba.push("salsa");
+prueba.push(35);
 
-prueba.push("nada");
+prueba.push(52);
 
-console.log(prueba[1]);
+//console.log(prueba[1]);
 
-console.log(prueba);
+//console.log(prueba);
 
-const even = (element) => element === "error";
+const even = prueba.map(function (x) {
+  return x * 2;
+});
 
-console.log(prueba.find(even));
+console.log(even);
 
 //prueba.push(5);
 
