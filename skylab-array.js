3e6e54ac3de26c517b9336503c3e9c55ec48d5b0
Sergio Garcia <sergio.gl86@gmail.com>
2021-10-07 19:23:10 +0200

    Creada el metodo some

--- skylab-array.js
+++ skylab-array.js
@@ -6,15 +6,46 @@ class skylabArray {
     this[this.length] = value;
     this.length = this.length + 1;
   }
+  some(value) {
+    if (this.length === 0) {
+      return false;
+    } else {
+      const someFunction = value;
+      for (let i = 0; i < this.length; i++) {
+        if (someFunction(this[i])) {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
 }
 
 const prueba = new skylabArray();
 
-console.log(prueba.length);
+prueba.push(5);
+
+prueba.push(9);
 
 prueba.push(5);
 
+prueba.push(8);
+
+console.log(prueba[1]);
+
 console.log(prueba);
+
+const even = (element) => element % 2 === 0;
+
+console.log(prueba.some(even));
+
+//prueba.push(5);
+
+//const algo = new Array();
+
+//console.log(algo.some( > 1));
+
+//console.log(prueba);
 
 /* const prueba = ['a', 'b', 'c'],
 
